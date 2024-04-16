d7f80cf3844f057491b5cc92becef56cf2e13abe
Marko <jthorn29@protonmail.com>
2024-04-16 11:43:45 +0200

    fix: add youtube videoembeddable param

--- lib/videos.ts
+++ lib/videos.ts
@@ -1,6 +1,6 @@
 export const getVideos = async(query: string, number: number) => {
   const searchTerm = query.replace(' ', '%20')
-  const response = await fetch(`https://youtube.googleapis.com/youtube/v3/search?part=snippet&type=video&maxResults=${number}&q=${searchTerm}&key=${process.env.YOUTUBE_API}`);
+  const response = await fetch(`https://youtube.googleapis.com/youtube/v3/search?part=snippet&type=video&maxResults=${number}&q=${searchTerm}&key=${process.env.YOUTUBE_API}&videoEmbeddable=true`);
 
   const data = await response.json()
 
